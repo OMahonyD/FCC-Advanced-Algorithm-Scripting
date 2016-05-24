be38bc198f5d86bcc0452c108eaf58c436906c9f
Dan O'Mahony <danomahony4@gmail.com>
2016-05-24 15:13:10 +0100

    Updated with totalCash and checks

--- 03 Exact Change.js
+++ 03 Exact Change.js
@@ -10,34 +10,49 @@ Here are some helpful links:
 Global Object
 **********************************************************/
 
+  
   function checkCashRegister(price, cash, cid) {
+  //Declare and initialise
+  var totalCash = 0;  
+    
   //Turn price and cash into pennies
   price *= 100;  cash *= 100;
 
   //Find change owed
-  var change = cash - price;
+  var changeOwed = cash - price;
   
   //Iterate through cid array
   for(var i in cid) {
     //Turn the values into pennies (Math.ceil used as one array returning floating point number)
     cid[i][1] = Math.ceil(cid[i][1] *= 100);
   }//
-  console.log("The change owed is " + change + " pennies");
-  console.log("The value of each denomination in pennies in drawer is " + cid);
+  
+  //Find totalCash
+  for(var j in cid) {
+    totalCash += cid[j][1];
+  }
 
-  
-  
-  }//end checkCashRegister
-  
   //if changeOwed > totalCash
+  if(changeOwed > totalCash) {
     //return the string "Insufficient Funds"
-  
-  //else if changeOwed === totalCash
+    return "Insufficient Funds";
+   //else if changeOwed === totalCash
+  }else if(changeOwed === totalCash) {
     //return the string "Closed"
-  
-  //else return an array of change denominations from highest to lowest
-  
-
+    return "Closed";
+  }else {
+     //else return an array of change denominations from highest to lowest
+  }//end if/else  
+      
+    
+    
+    
+    
+    
+  console.log("Total Cash is: " + totalCash);  
+  console.log("The change owed is " + changeOwed + " pennies");
+  console.log("The value of each denomination in pennies in drawer is " + cid);
+  }//end checkCashRegister
 
 // Example cash-in-drawer array:
 // [["PENNY", 1.01],
@@ -51,4 +66,3 @@ Global Object
 // ["ONE HUNDRED", 100.00]]
 
 checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
-
